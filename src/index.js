b453d73a2483b3e0b03f1785a495b28c857a702d
Chadwell Clark <wcclark23@comcast.net>
2021-10-30 23:26:45 -0500

    Added Graphql can add songs to Queue now!

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
+import { ApolloProvider } from "@apollo/react-hooks";
 import theme from "./theme";
+import client from "./graphql/client";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <ApolloProvider client={client}>
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
       <App />
     </MuiThemeProvider>
-  </React.StrictMode>,
+  </ApolloProvider>,
+  // </React.StrictMode>,
   document.getElementById("root")
 );
